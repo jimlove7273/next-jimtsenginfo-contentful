160901e7c9d50255a6465c2cf7cdecb0e8377028
Jim Tseng <jimlove@myehouse.com>
2022-01-16 14:57:42 -0800

    Remove a problem in 404 page

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ export default function Skeleton() {
 			<div className="container">
         <div className="pgcontent">
 					404<br />
-					OOPS, SORRY I CAN'T FIND THAT PAGE!<br />
+					OOPS, SORRY I CANNOT FIND THAT PAGE!<br />
 					Please check your entry in the URL and try again..<br />
 				</div>
 			</div>
