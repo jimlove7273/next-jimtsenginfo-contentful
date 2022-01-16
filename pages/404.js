906cb2f193ce73d7e49082ea697d4ab1eec0b029
Jim Tseng <jimlove@myehouse.com>
2022-01-16 15:19:26 -0800

    Update the 404 page

--- pages/404.js
+++ pages/404.js
@@ -6,10 +6,33 @@ export default function Skeleton() {
 			<Layout>
 			<div className="container">
         <div className="pgcontent">
-					404<br />
-					OOPS, SORRY I CANNOT FIND THAT PAGE!<br />
-					Please check your entry in the URL and try again..<br />
+					<div className="block404">
+						<div className="b404number">404</div>
+						<div className="b404line1">OOPS, SORRY I CANNOT FIND THAT PAGE!</div>
+						<div className="b404line2">Please check your entry in the URL and try again..</div>
+					</div>
 				</div>
+				<style jsx>{`
+					.block404 {
+						display: flex;
+						flex-direction: column;
+						padding: 100px 0;
+						align-items: center;
+					}
+					.b404number {
+						color: #179153;
+						font-size: 60px;
+					}
+					.b404line1 {
+						color: #2a4c6e;
+						font-size: 14px;
+						padding: 20px 0;
+					}
+					.b404line2 {
+						color: #778593;
+						font-size: 16px;
+					}
+				`}</style>
 			</div>
 			</Layout>
 		</div>
