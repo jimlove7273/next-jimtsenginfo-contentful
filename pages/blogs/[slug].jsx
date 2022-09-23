d6e7fe101e7cb9b4ba806ddb683e65b7212f49c8
Jim Tseng <jimlove@myehouse.com>
2022-09-22 21:59:35 -0700

    Clean Up the Portfolio and Blog single page, remove the extra return

--- pages/blogs/[slug].jsx
+++ pages/blogs/[slug].jsx
@@ -82,11 +82,9 @@ export const getStaticProps = async ({params}) => {
 export const getStaticPaths = async () => {
 
 	const res = await client.getEntries({ content_type: 'jtiBlogs'})
-	const paths = res.items.map(item => {
-		return {
+	const paths = res.items.map(item => ({
 			params: { slug: item.fields.slug }
-		}
-	})
+	}))
 
 	return {
 		paths,
